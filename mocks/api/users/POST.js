ea47221dfd3cfb093843d7d56ecb39b4a427402a
Ludwing Badillo <ldominguezgb@sofi.org>
2019-12-15 12:29:43 -0500

    cambiando lso mocks

--- mocks/api/users/POST.js
+++ mocks/api/users/POST.js
@@ -1,6 +1,5 @@
 module.exports = function (request, response) {
-    if (request.body.hola === "quemas") {
-    console.log(request.body.hola)
+    if (request.body.dato === "ejemplo") {
         response.status(403).send({});
     } else {
         response.sendFile('POST.json', {root: __dirname});
